c2b49133e7fe5adce52f278a959ac22e7a13ca3b
Jon Weber <webs05@gmail.com>
2019-04-28 10:17:28 -0500

    just some more python playing

---
+++ main.py
@@ -0,0 +1,19 @@
+5+6
+print('She said "Don\'t Do that"')
+"Hello, " + "Nick"
+"This costs " + str(6) + " dollars"
+"This costs " + str(6 + 5) + " dollars"
+"This costs " + "$" + str(6 + 5) + " dollars"
+"Hello:Nick"
+"Hello:Nick:Jon:Rami".split(":")
+"My Name is " + "Hello:Nick:Jon:Rami".split(":")[2]
+True
+5 == 5
+5 == 4
+5 is 5
+5 is not 5
+5 is not 6
+"True" is True
+"True" is str(True)
+"This is " + "really" + str(" cool")
+print("Hello Jonny Boy")
